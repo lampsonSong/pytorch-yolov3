d5e7ce51748ed5c35a5b31495f5a74aac30bae77
lampson <lampson.song@ulsee.com>
2020-04-18 20:45:52 +0800

    test coco with only one device when distributed training

--- src/train.py
+++ src/train.py
@@ -26,13 +26,13 @@ from utils.utils import non_max_suppression, scale_coords, clip_coords
 from coco_eval import convert_out_format, save_json, get_coco_eval, coco80_to_coco91_class
 
 hyp = {
-        'weight_decay' : 0.0,
-        'reg_loss_gain' : 0.5,
-        'obj_loss_gain' : 8.0,
-        'cls_loss_gain' : 20.0,
-        'train_iou_thresh' : 0.225,
-        'fl_gamma' : 2.
-    }
+    'weight_decay' : 0.00484,
+    'reg_loss_gain' : 0.54,
+    'obj_loss_gain' : 64.3,
+    'cls_loss_gain' : 37.4,
+    'train_iou_thresh' : 0.225,
+    'fl_gamma' : 2.
+}
 
 def get_dataloader(args, local_rank):
     if not args.test_only:
@@ -43,12 +43,12 @@ def get_dataloader(args, local_rank):
                 img_size=args.img_size,
                 multiscale=args.multi_scale_training,
                 phase='Train'
-            )
+                )
         train_sampler = DistributedSampler(
                 train_dataset,
                 num_replicas = args.world_size,
                 rank = local_rank
-            )
+                )
         train_loader = DataLoader(
                 dataset = train_dataset, 
                 batch_size = args.batch_size,
@@ -57,15 +57,15 @@ def get_dataloader(args, local_rank):
                 pin_memory = True,
                 sampler = train_sampler,
                 collate_fn = train_dataset.collate_fn
-            )
+                )
 
-    # test dataloader
+        # test dataloader
     test_dataset = COCODatasetYolo(
             coco_dir=args.coco_dir, 
             set_name='val2017', 
             img_size=args.img_size, 
             phase='Test'
-        )
+            )
     # number of classes from dataset
     args.num_classes = test_dataset.num_classes
     test_sampler = DistributedSampler(
@@ -75,13 +75,13 @@ def get_dataloader(args, local_rank):
             )
     test_loader = DataLoader(
             dataset = test_dataset, 
-            batch_size = args.batch_size,
+            batch_size = 1,
             num_workers = args.num_workers,
             shuffle = False,
             pin_memory = True,
-            sampler = test_sampler,
+            #sampler = test_sampler, # only one device is used to test
             collate_fn = test_dataset.collate_fn
-        )
+            )
 
     if args.test_only:
         return test_loader
@@ -116,7 +116,7 @@ def train_yolo(gpu, args):
     model = YOLOv3_SPP(num_classes = args.num_classes)
     torch.cuda.set_device(gpu)
     model.cuda(gpu)
-   
+
     # add optimizer to parameters of the model
     param_g0, param_g1, param_g2 = [], [], []
     for k, v in dict(model.named_parameters()).items():
@@ -127,16 +127,16 @@ def train_yolo(gpu, args):
         else:
             param_g0 += [v]
 
-    ## optimizer use sgd
-    #optimizer = optim.SGD(param_g0, lr=args.lr, momentum=args.momentum)
+    # optimizer use sgd
+    optimizer = optim.SGD(param_g0, lr=args.lr, momentum=args.momentum)
     ## optimizer use adam
-    optimizer = optim.Adam(param_g0, lr=args.lr)
-    
+    #optimizer = optim.Adam(param_g0, lr=args.lr)
+
     optimizer.add_param_group({'params': param_g1, 'weight_decay': hyp['weight_decay']})
     optimizer.add_param_group({'params': param_g2})
     optimizer.param_groups[2]['lr'] *= 2.0 # bias learning rate
     del param_g0, param_g1, param_g2
-    
+
     if args.mixed_precision:
         model, optimizer = apex.amp.initialize(model, optimizer, opt_level='O1', verbosity=0)
         model = apex.parallel.DistributedDataParallel(model, delay_allreduce=True)
@@ -147,8 +147,8 @@ def train_yolo(gpu, args):
                     device_ids=[gpu],
                     find_unused_parameters=True)
 
-    start_epoch = 0
-    
+            start_epoch = 0
+
     # resume TODO
     if args.resume and os.path.exists(args.resume):
         checkpoint = torch.load(args.resume, map_location="cuda:{}".format(local_rank))
@@ -160,8 +160,8 @@ def train_yolo(gpu, args):
 
         start_epoch = checkpoint['epoch']
 
-    
-    
+
+
     # scheduler, cosine 
     lr_func = lambda x: (1 + math.cos(x * math.pi / args.epoches)) / 2 * 0.99 + 0.01 
     scheduler =  lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_func)
@@ -183,7 +183,7 @@ def train_yolo(gpu, args):
             for idx, (imgs, targets, _, _) in pbar:
                 if(len(targets) == 0):
                     continue
-                
+
                 ni = idx + epoch * len(train_loader)
 
                 imgs = imgs.cuda(gpu, non_blocking=True).float() / 255.
@@ -198,7 +198,7 @@ def train_yolo(gpu, args):
                 if not torch.isfinite(loss):
                     print('WARNING: non-finite loss, ending training ', loss_items)
                     return results
-                
+
                 loss /= args.accumulate
 
                 if args.mixed_precision:
@@ -215,8 +215,9 @@ def train_yolo(gpu, args):
                 # mean loss
                 mean_loss = (mean_loss * idx + loss_items.cpu()) / (idx + 1) 
                 mem = '%.3gG' % (torch.cuda.memory_cached() / 1E9 if torch.cuda.is_available() else 0)  # (GB)
-                s = ('%10s' * 2 + '%10.3g' * 6) % ('%g/%g' % (epoch, args.epoches - 1), mem, *mean_loss, len(targets), max(imgs[0].shape[2:]))
-                
+                #s = ('%10s' * 2 + '%10.3g' * 6) % ('%g/%g' % (epoch, args.epoches - 1), mem, *mean_loss, len(targets), max(imgs[0].shape[2:]))
+                s = ('%10s' * 2 + '%10.3g' * 6) % ('%g/%g' % (epoch, args.epoches - 1), mem, *loss_items.cpu(), len(targets), max(imgs[0].shape[2:]))
+
                 pbar.set_description(s)
 
             # update scheduler
@@ -228,70 +229,85 @@ def train_yolo(gpu, args):
         2. save the better model
         '''
         # test coco
-        model.eval()
-        test_input_shape = (416, 416)
-        results = []
-        processed_ids = []
-        coco91cls = coco80_to_coco91_class()
-        tbar = tqdm(enumerate(test_loader), total=len(test_loader))
-        for idx, (imgs, targets, orig_imgs_tuple, img_id_tuple) in tbar:
-            if(len(targets) == 0):
-                continue
+        test_device = next(model.parameters()).device
+        if str(test_device) == "cuda:0":
+            test_model = YOLOv3_SPP(num_classes = args.num_classes)
+            test_model.to(test_device)
             
-            imgs = imgs.cuda(gpu, non_blocking=True).float() / 255.
-            targets = targets.cuda(gpu, non_blocking=True)
-
-            # run model
-            with torch.no_grad():
-                thres_out = 0.5
-                yolo_outs = model(imgs)
-                outputs = non_max_suppression(yolo_outs, conf_thres=thres_out)
-                for i, det in enumerate(outputs):
-                    if det == None:
-                        continue
-                    
-                    orig_img = orig_imgs_tuple[i]
-                    det[:, :4] = scale_coords(test_input_shape, det[:, :4], orig_img.shape).round()
-                    img_result = convert_out_format(img_id_tuple[i], det, coco91cls, thres_out)
-                    if img_result:
-                        processed_ids.append(img_id_tuple[i])
-                        #results.append(item for item in list(img_result))
-                        for item in list(img_result):
-                            results.append(item)
-                    #print(results)
-                    ### uncommet to vis results
-                    #import cv2
-                    #for x1, y1, x2, y2, conf, cls in det:
-                    #    cv2.rectangle( orig_img, (x1, y1), (x2, y2), (0,0,255), 2)
-
-                    #cv2.imshow("- i", orig_img)
-                    #cv2.waitKey(0)
-
-        # save results for new test
-        pred_file = "../results/val2017_bbox_results.json"
-        save_json(pred_file, results)
-        val_file = os.path.join(args.coco_dir,'annotations/instances_val2017.json')
-        
-        test_status = get_coco_eval(val_file, pred_file, processed_ids)
-        
-        if args.test_only:
-            continue
-        else:
-            # save model
-            if local_rank == 0 and not test_status[0] > best_mAP:
-                best_mAP = test_status[0]
+            if args.test_only:
+                checkpoint = torch.load(args.resume, map_location=test_device)
+                test_model.load_state_dict(checkpoint['state_dict'])
+            else:
                 if args.world_size > 1:
-                    state_dict = model.module.state_dict()
+                    test_model.load_state_dict(model.module.state_dict())
                 else:
-                    state_dict = model.state_dict()
-                
-                state = {
-                        'state_dict' : state_dict,
-                        #'optimizer' : optimizer.state_dict(),
-                        'epoch' : epoch,
-                        #'scheduler' : scheduler.state_dict()
-                        }
-                torch.save(state, "../weights/yolov3_epoch{}.pth".format(epoch))
+                    test_model.load_state_dict(model.state_dict())
+            
+            test_model.eval()
+            test_input_shape = (416, 416)
+            results = []
+            processed_ids = []
+            coco91cls = coco80_to_coco91_class()
+            tbar = tqdm(enumerate(test_loader), total=len(test_loader))
+            for idx, (imgs, targets, orig_imgs_tuple, img_id_tuple) in tbar:
+                if(len(targets) == 0):
+                    continue
+
+                imgs = imgs.cuda(gpu, non_blocking=True).float() / 255.
+                targets = targets.cuda(gpu, non_blocking=True)
+
+                # run model
+                with torch.no_grad():
+                    thres_out = 0.05
+                    yolo_outs = test_model(imgs)
+                    outputs = non_max_suppression(yolo_outs, conf_thres=thres_out)
+                    for i, det in enumerate(outputs):
+                        if det == None:
+                            print("det is none")
+                            continue
+
+                        orig_img = orig_imgs_tuple[i]
+                        det[:, :4] = scale_coords(test_input_shape, det[:, :4], orig_img.shape).round()
+                        img_result = convert_out_format(img_id_tuple[i], det, coco91cls, thres_out)
+                        if img_result:
+                            processed_ids.append(img_id_tuple[i])
+                            #results.append(item for item in list(img_result))
+                            for item in list(img_result):
+                                results.append(item)
+                        ##print(results)
+                        ## uncommet to vis results
+                        #import cv2
+                        #for x1, y1, x2, y2, conf, cls in det:
+                        #    cv2.rectangle( orig_img, (x1, y1), (x2, y2), (0,0,255), 2)
+
+                        #cv2.imshow("- i", orig_img)
+                        #cv2.waitKey(0)
+
+            # save results for new test
+            pred_file = "../results/val2017_bbox_results.json"
+            save_json(pred_file, results)
+            val_file = os.path.join(args.coco_dir,'annotations/instances_val2017.json')
+
+            test_status = get_coco_eval(val_file, pred_file, processed_ids)
+
+            if args.test_only:
+                continue
+            else:
+                # save model
+                if local_rank == 0 and not test_status[0] > best_mAP:
+                    best_mAP = test_status[0]
+                    if args.world_size > 1:
+                        state_dict = model.module.state_dict()
+                    else:
+                        state_dict = model.state_dict()
+
+                    state = {
+                            'state_dict' : state_dict,
+                            #'optimizer' : optimizer.state_dict(),
+                            'epoch' : epoch,
+                            #'scheduler' : scheduler.state_dict()
+                            }
+                    torch.save(state, "../weights/yolov3_epoch{}.pth".format(epoch))
 
 
 
@@ -309,7 +325,7 @@ if __name__ == "__main__":
     parser.add_argument('--lr', type=float, default='1e-3')
     parser.add_argument('--momentum', type=float, default='0.99')
     parser.add_argument('--multi_scale_training', type=bool, default=True)
-    parser.add_argument('--mixed_precision', type=bool, default=False)
+    parser.add_argument('--mixed_precision', type=bool, default=True)
     parser.add_argument('--test_only', type=bool, default=False)
     parser.add_argument('--accumulate', type=int, default=4, help='accumulate 4 times then optimizing')
     args = parser.parse_args()
